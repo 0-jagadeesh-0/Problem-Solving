264d70f0d3d4a9d182bc6d541a5afe5f67a9bf06
Jagadeesh <74336885+0-jagadeesh-0@users.noreply.github.com>
2022-07-02 02:03:49 +0530

    Update preorder.cpp

--- Trees/preorder.cpp
+++ Trees/preorder.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+using namespace std;
 
+class TreeNode{
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+}
+
+// Recurssion
+
+void preOrder(TreeNode* root)
+{
+  if(root==NULL) return;
+  cout<<root->val<<" ";
+  preOrder(root->left);
+  preOrder(root->right);
+  
+}
