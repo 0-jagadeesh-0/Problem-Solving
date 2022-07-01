d6a59952d239bacbeb5fabb9633a1bd42e80bfc4
Jagadeesh <74336885+0-jagadeesh-0@users.noreply.github.com>
2022-07-02 02:02:12 +0530

    Update postorder.cpp

--- Trees/postorder.cpp
+++ Trees/postorder.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+using namespace std;
 
+class TreeNode{
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+}
+
+// Recurssion
+
+void postOrder(TreeNode* root)
+{
+  if(root==NULL) return;
+  postOrder(root->left);
+  cout<<root->val<<" ";
+  postOrder(root->right);
+}
