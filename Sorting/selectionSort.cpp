9633b64bf99fd7c82b5f097956040f29102553be
Jagadeesh <74336885+0-jagadeesh-0@users.noreply.github.com>
2022-07-02 13:04:10 +0530

    Update selectionSort.cpp

--- Sorting/selectionSort.cpp
+++ Sorting/selectionSort.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+using namespace std;
 
+void selectionSort(int arr[],int n)
+{
+  int mini;
+  for(int i=0;i<n;i++)
+  {
+    mini = i;
+    for(int j=i+1;j<n;j++)
+    {
+      if(arr[j]<arr[i]) mini = j;
+    }
+    int temp = arr[i];
+    arr[i] = arr[mini];
+    arr[mini] = temp;
+  }
+}
+
+// Time Complexity is O(N*N).
