510104c57818ddbb801d03bca985fb3855b67a37
Jagadeesh <74336885+0-jagadeesh-0@users.noreply.github.com>
2022-07-02 09:21:48 +0530

    Update middleElement.cpp

--- Linkedlist/middleElement.cpp
+++ Linkedlist/middleElement.cpp
@@ -1,1 +1,20 @@
+// Find the middle element in the linkedlist.
 
+#include<iostream>
+using namespace std;
+
+Node* middleElement(Node* root)
+{
+  Node* fast = root;
+  Node* slow = root;
+  while(fast->next&&fast->next->next)
+  {
+    slow=slow->next;
+    fast=fast->next->next;
+  }
+  if(fast->next) return slow->next;
+  return slow;
+  
+}
+
+// Time Complexity is O(N)
